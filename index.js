9331636aab90a9bff63250217717290c39f0e7f3
rafavd <rafarovd@gmail.com>
2026-03-09 19:24:53 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,44 +1,60 @@
-// https://[url]/run?pass=[senha]
-
-import express from "express";
-import fetch from "node-fetch";
-
+const express = require("express");
 const app = express();
 
-const USER = "rafarvd";
-const REPO = "docker";
-const WORKFLOW = "run.yml";
+const WORKFLOW = "main.yml";
+const GH_TOKEN = process.env.GH_TOKEN;
+const PORT = process.env.PORT || 3000;
 
-app.get("/run", async (req, res) => {
-  const { pass } = req.query;
-
-  // 🔐 senha simples
-  if (pass !== process.env.RUN_PASSWORD) {
-    return res.status(401).send("Senha incorreta");
-  }
-
+app.get("/run/:user/:repo/:min/:max", async (req, res) => {
   try {
-    await fetch(
-      `https://api.github.com/repos/${USER}/${REPO}/actions/workflows/${WORKFLOW}/dispatches`,
-      {
-        method: "POST",
-        headers: {
-          Authorization: `Bearer ${process.env.GH_TOKEN}`,
-          Accept: "application/vnd.github+json"
-        },
-        body: JSON.stringify({
-          ref: "main"
-        })
-      }
-    );
+    const user = req.params.user;
+    const repoPrefix = req.params.repo;
+    const min = Number(req.params.min);
+    const max = Number(req.params.max);
 
-    res.send("✅ Workflow disparado!");
+    if (max - min > 50) {
+      return res.send("Limite máximo 50 repos");
+    }
+
+    if (!Number.isInteger(min) || !Number.isInteger(max)) {
+      return res.send("min e max inválidos");
+    }
+
+    if (max < min) {
+      return res.send("max precisa ser maior que min");
+    }
+
+    const jobs = [];
+
+    for (let i = min; i <= max; i++) {
+      const repo = `${repoPrefix}${i}`;
+
+      jobs.push(
+        fetch(
+          `https://api.github.com/repos/${user}/${repo}/actions/workflows/${WORKFLOW}/dispatches`,
+          {
+            method: "POST",
+            headers: {
+              Authorization: `Bearer ${GH_TOKEN}`,
+              Accept: "application/vnd.github+json",
+            },
+            body: JSON.stringify({ ref: "main" }),
+          },
+        ),
+      );
+    }
+
+    await Promise.all(jobs);
+
+    res.send(
+      `✅ Workflows disparados ${user}/${repoPrefix}${min}-${repoPrefix}${max}`,
+    );
   } catch (err) {
     console.log(err);
-    res.status(500).send("Erro ao disparar");
+    res.status(500).send("Erro");
   }
 });
 
-app.listen(process.env.PORT || 3000, () =>
-  console.log("Servidor rodando")
-);
+app.listen(PORT, () => {
+  console.log("Servidor rodando...");
+});
